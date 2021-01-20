e88e2bbb108694e918efa279aa9f474f9ab3b1fd
catcherindarah <22rray@athenian.org>
2021-01-20 06:52:15 +0000

    I wrote questions as comments, as I still had trouble on the last two

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,25 +2,37 @@ public class MyMain {
 
     // Returns the mean of the 2D array mat
     public static double mean(double[][] mat) { 
-        // YOUR CODE HERE
-        return -1.0;
+        int counter=0;
+        double sum = 0;
+        for(int i=0;i<mat.length;i++)
+        {
+            for(int j=0;j<mat[i].length;j++)
+            {
+                sum = sum+mat[i][j];
+                counter++;
+            }
+        }
+        return sum/counter;
     }
 
     // Returns the median of the 2D array mat
     public static double median(double[][] mat) { 
-        // YOUR CODE HERE
-        return -1.0;
+        // Create a counter that adds up the total number of items in array (similar to above)
+        // Divide that by 2 because the halfway point is the median
+        // How do I know what row and column that halfway point is? Can I use getValue() method?
     }
     
 
     // Returns the mode of the 2D array mat
     public static double mode(double[][] mat) { 
-        // YOUR CODE HERE
-        return -1.0;
+        // I think a counter would be useful here but how do I create seperate counters for each unique number?
+        // I could use these counters to then say that the counter with the highest value is the mode.. 
+        // or rather the number which cause the counter to increase is the mode
+        
     }
 
 
     public static void main(String[] args) {
-        // Write some code here to test your methods!
+        System.out.println(mean(double[][]mat));
     }
 }
